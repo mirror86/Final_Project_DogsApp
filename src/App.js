6dda1339749f613e7ce315aa9bfed9e92225fff9
mirror86 <mira.wolicka@gmail.com>
2023-07-04 11:39:15 +0200

    adding pages and components , App.js - test ride

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
-git
+import {Button} from "react-bootstrap";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+<Button variant={"primary"}>Hej kliknij we mnie</Button>
 
       </header>
     </div>

42ff4b6432c54d4710d5e4c69ff124cfbf3fb2a1
mirror86 <mira.wolicka@gmail.com>
2023-07-10 19:12:50 +0200

    using fetch REST API

--- src/components/BreedCard/BreedCard.js
+++ src/components/BreedCard/BreedCard.js
@@ -1,25 +1,44 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {Button, Card, Col} from "react-bootstrap";
 
+const apiUrl = "https://api.api-ninjas.com/v1/dogs?name=";
+const breedName = `[].jpg`; // z małej listyety i z podłogą
+const apiKey = "Puy2zDCkC5Og1tl/a9AkYQ==duVwVJvXcT0MnzNG";
 const BreedCard = () => {
+    const [data, setData] = useState(false);
+    const [breedName, setBreedName] = useState("");
+    const [breedImg, setBreedImg] = useState("");
+
+    useEffect(() => {
+        fetch(`${apiUrl}${breedName}`, {
+            method: "GET",
+            headers: {
+                'X-Api-Key': apiKey,
+                'Content-Type': 'application/json'
+            }
+        })
+            .then(response => response.json())
+            .then(data => setData(data))
+            .catch(err =>
+                console.error(err))
+    }, [])
+
     return (
         <>
-        <Col xl={3} md={4} sm={6} className="">
-            <Card className="border-light shadow-lg mt-5 mb-5 ms-2 me-2 ">
-                <Card.Img variant="top" src="/IMG-2713.jpg"/>
-                <Card.Body>
-                    <Card.Title>Card Title</Card.Title>
-                    <i className="fa-regular fa-heart"></i>
-                    <Card.Text>
-                        Some quick example text to build on the card title and make up the
-                        bulk of the card's content.
-                    </Card.Text>
+            <Col xl={3} md={4} sm={6} className="">
+                <Card className="border-light shadow-lg mt-5 mb-5 ms-2 me-2 ">
+                    <Card.Img variant="top" src="/IMG-2713.jpg"/>
+                    <Card.Body className="d-flex justify-content-between">
+
+                            <Card.Title>Card Title</Card.Title>
+                            <i className="fa-regular fa-heart"></i>
+
+                    </Card.Body>
                     <Button variant="outline-info">More</Button>
-                </Card.Body>
-            </Card>
-        </Col>
+                </Card>
+            </Col>
         </>
-    );
+);
 };
 
 export default BreedCard;
--- src/pages/DogList/DogList.js
+++ src/pages/DogList/DogList.js
@@ -1,10 +1,11 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import Container from "react-bootstrap/Container";
-import {Button, Row} from "react-bootstrap";
+import {Row} from "react-bootstrap";
 import BreedCard from "../../components/BreedCard/BreedCard";
 
-
 const DogList = () => {
+
+
     return (
         <>
             <Container fluid className="pages__container main h-100 shadow-lg bg-white " style={{overflowY: "scroll"}}>

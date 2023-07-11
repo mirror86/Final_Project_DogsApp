34953c13fbfac632d916afe32e979b60704d094e
mirror86 <mira.wolicka@gmail.com>
2023-07-11 20:02:44 +0200

    Added FavDogElement as independent component

--- src/pages/FavDogs/FavDogs.js
+++ src/pages/FavDogs/FavDogs.js
@@ -1,84 +1,16 @@
 import React from 'react';
 import Container from "react-bootstrap/Container";
-
 import {Col, Row} from "react-bootstrap";
-
-
+import FavDogElement from "../../components/FavDogElement/FavDogElement";
 const FavDogs = () => {
     return (
         <>
             <Container fluid className="pages__container main h-100 shadow-lg  bg-white" style={{overflowY: "scroll"}} >
                <Container fluid className="h-100 bg-white">
-                        <Row className=" ">
-                            <Col>
-                                <Container fluid="lg" className="mt-4">
-                                    <Row className=" shadow justify-content-evenly gap-4 m-sm-2 align-items-center pt-2 pb-2">
-                                        <Col xs={12} sm={12} md={4}  lg={3} xl={3} className="text-center"><img src="/dog-icon.jpg" alt="" className=" w-100 rounded cover"/></Col>
-                                        <Col xs={12} sm={12} md={6} lg={6} xl={6} className=""><strong>Dog Breed</strong>
-                                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus accusantium culpa doloribus eos, ex explicabo hic nam possimus unde voluptatem.</Col>
-                                        <Col xs={12} sm={12} md={1} lg={2} xl={2} className="text-center">action <i className="fa-solid fa-trash-can" /></Col>
-                                    </Row>
-                                    <Row className=" shadow justify-content-evenly gap-4 m-sm-2 align-items-center pt-2 pb-2">
-                                        <Col xs={12} sm={12} md={4}  lg={3} xl={3} className="text-center"><img src="/dog-icon.jpg" alt="" className=" w-100 rounded cover"/></Col>
-                                        <Col xs={12} sm={12} md={6} lg={6} xl={6} className=""><strong>Dog Breed</strong>
-                                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus accusantium culpa doloribus eos, ex explicabo hic nam possimus unde voluptatem.</Col>
-                                        <Col xs={12} sm={12} md={1} lg={2} xl={2} className="text-center">action <i className="fa-solid fa-trash-can" /></Col>
-                                    </Row>
-                                    <Row className=" shadow justify-content-evenly gap-4 m-sm-2 align-items-center pt-2 pb-2">
-                                        <Col xs={12} sm={12} md={4}  lg={3} xl={3} className="text-center"><img src="/dog-icon.jpg" alt="" className=" w-100 rounded cover"/></Col>
-                                        <Col xs={12} sm={12} md={6} lg={6} xl={6} className=""><strong>Dog Breed</strong>
-                                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus accusantium culpa doloribus eos, ex explicabo hic nam possimus unde voluptatem.</Col>
-                                        <Col xs={12} sm={12} md={1} lg={2} xl={2} className="text-center">action <i className="fa-solid fa-trash-can" /></Col>
-                                    </Row>
-                                    <Row className=" shadow justify-content-evenly gap-4 m-sm-2 align-items-center pt-2 pb-2">
-                                        <Col xs={12} sm={12} md={4}  lg={3} xl={3} className="text-center"><img src="/dog-icon.jpg" alt="" className=" w-100 rounded cover"/></Col>
-                                        <Col xs={12} sm={12} md={6} lg={6} xl={6} className=""><strong>Dog Breed</strong>
-                                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus accusantium culpa doloribus eos, ex explicabo hic nam possimus unde voluptatem.</Col>
-                                        <Col xs={12} sm={12} md={1} lg={2} xl={2} className="text-center">action <i className="fa-solid fa-trash-can" /></Col>
-                                    </Row>
-                                    <Row className=" shadow justify-content-evenly gap-4 m-sm-2 align-items-center pt-2 pb-2">
-                                        <Col xs={12} sm={12} md={4}  lg={3} xl={3} className="text-center"><img src="/dog-icon.jpg" alt="" className=" w-100 rounded cover"/></Col>
-                                        <Col xs={12} sm={12} md={6} lg={6} xl={6} className=""><strong>Dog Breed</strong>
-                                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus accusantium culpa doloribus eos, ex explicabo hic nam possimus unde voluptatem.</Col>
-                                        <Col xs={12} sm={12} md={1} lg={2} xl={2} className="text-center">action <i className="fa-solid fa-trash-can" /></Col>
-                                    </Row>
-                                    <Row className=" shadow justify-content-evenly gap-4 m-sm-2 align-items-center pt-2 pb-2">
-                                        <Col xs={12} sm={12} md={4}  lg={3} xl={3} className="text-center"><img src="/dog-icon.jpg" alt="" className=" w-100 rounded cover"/></Col>
-                                        <Col xs={12} sm={12} md={6} lg={6} xl={6} className=""><strong>Dog Breed</strong>
-                                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus accusantium culpa doloribus eos, ex explicabo hic nam possimus unde voluptatem.</Col>
-                                        <Col xs={12} sm={12} md={1} lg={2} xl={2} className="text-center">action <i className="fa-solid fa-trash-can" /></Col>
-                                    </Row>
-                                    <Row className=" shadow justify-content-evenly gap-4 m-sm-2 align-items-center pt-2 pb-2">
-                                        <Col xs={12} sm={12} md={4}  lg={3} xl={3} className="text-center"><img src="/dog-icon.jpg" alt="" className=" w-100 rounded cover"/></Col>
-                                        <Col xs={12} sm={12} md={6} lg={6} xl={6} className=""><strong>Dog Breed</strong>
-                                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus accusantium culpa doloribus eos, ex explicabo hic nam possimus unde voluptatem.</Col>
-                                        <Col xs={12} sm={12} md={1} lg={2} xl={2} className="text-center">action <i className="fa-solid fa-trash-can" /></Col>
-                                    </Row>
-                                    <Row className=" shadow justify-content-evenly gap-4 m-sm-2 align-items-center pt-2 pb-2">
-                                        <Col xs={12} sm={12} md={4}  lg={3} xl={3} className="text-center"><img src="/dog-icon.jpg" alt="" className=" w-100 rounded cover"/></Col>
-                                        <Col xs={12} sm={12} md={6} lg={6} xl={6} className=""><strong>Dog Breed</strong>
-                                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus accusantium culpa doloribus eos, ex explicabo hic nam possimus unde voluptatem.</Col>
-                                        <Col xs={12} sm={12} md={1} lg={2} xl={2} className="text-center">action <i className="fa-solid fa-trash-can" /></Col>
-                                    </Row>
-                                    <Row className=" shadow justify-content-evenly gap-4 m-sm-2 align-items-center pt-2 pb-2">
-                                        <Col xs={12} sm={12} md={4}  lg={3} xl={3} className="text-center"><img src="/dog-icon.jpg" alt="" className=" w-100 rounded cover"/></Col>
-                                        <Col xs={12} sm={12} md={6} lg={6} xl={6} className=""><strong>Dog Breed</strong>
-                                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus accusantium culpa doloribus eos, ex explicabo hic nam possimus unde voluptatem.</Col>
-                                        <Col xs={12} sm={12} md={1} lg={2} xl={2} className="text-center">action <i className="fa-solid fa-trash-can" /></Col>
-                                    </Row>
-                                    <Row className=" shadow justify-content-evenly gap-4 m-sm-2 align-items-center pt-2 pb-2">
-                                        <Col xs={12} sm={12} md={4}  lg={3} xl={3} className="text-center"><img src="/dog-icon.jpg" alt="" className=" w-100 rounded cover"/></Col>
-                                        <Col xs={12} sm={12} md={6} lg={6} xl={6} className=""><strong>Dog Breed</strong>
-                                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus accusantium culpa doloribus eos, ex explicabo hic nam possimus unde voluptatem.</Col>
-                                        <Col xs={12} sm={12} md={1} lg={2} xl={2} className="text-center">action <i className="fa-solid fa-trash-can" /></Col>
-                                    </Row>
-                                    <Row className=" shadow justify-content-evenly gap-4 m-sm-2 align-items-center pt-2 pb-2">
-                                        <Col xs={12} sm={12} md={4}  lg={3} xl={3} className="text-center"><img src="/dog-icon.jpg" alt="" className=" w-100 rounded cover"/></Col>
-                                        <Col xs={12} sm={12} md={6} lg={6} xl={6} className=""><strong>Dog Breed</strong>
-                                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus accusantium culpa doloribus eos, ex explicabo hic nam possimus unde voluptatem.</Col>
-                                        <Col xs={12} sm={12} md={1} lg={2} xl={2} className="text-center">action <i className="fa-solid fa-trash-can" /></Col>
-                                    </Row>
-
+                   <Row className=" ">
+                       <Col>
+                           <Container fluid="lg" className="mt-4">
+                                    <FavDogElement/>
                                 </Container>
                             </Col>
                         </Row>

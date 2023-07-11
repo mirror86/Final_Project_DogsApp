832ff5d611acf3fb90e10df64957497a3d71288d
mirror86 <mira.wolicka@gmail.com>
2023-07-11 11:44:56 +0200

    added NeXt and Prev functionalities to Dogs List (test mode)

--- src/components/BreedCard/BreedCard.js
+++ src/components/BreedCard/BreedCard.js
@@ -1,19 +1,14 @@
 import React, {useEffect, useState} from 'react';
-import {Button, Card, Col} from "react-bootstrap";
-
+import {Button, Card, Col, Spinner} from "react-bootstrap";
 
 const apiUrl = "https://api.api-ninjas.com/v1/dogs?min_height=1";
 const apiKey = "Puy2zDCkC5Og1tl/a9AkYQ==duVwVJvXcT0MnzNG";
 const BreedCard = () => {
     const [dogData, setDogData] = useState(false);
-const [iconState, setIconState] = useState(false)
-const onHandleIconClick =() => {
-    setIconState(!iconState);
-
-}
-
-    useEffect(() => {
-        fetch(`${apiUrl}`, {
+    const [siteNumber, setSiteNumber] = useState(1);
+    const [itsFavourites, setItsFavourites] = useState(false); // albo false albo pusta tablica favourites uzyj !
+    const loadData = (offset) => {
+        fetch(`${apiUrl}&offset=${offset}`, {
             method: "GET",
             headers: {
                 'X-Api-Key': apiKey,
@@ -24,23 +19,55 @@ const onHandleIconClick =() => {
             .then(data => setDogData(data))
             .catch(err =>
                 console.error(err))
-    }, [])
+    }
 
-    return (
+const onHandleIconClick = () => {
+    setItsFavourites(!itsFavourites);
+}
+    const handleClickNextButton = () => {
+        setSiteNumber(prevState => prevState + 1)
+        const offset = siteNumber * 20
+        loadData(offset);
+    }
+
+    const handleClickPrevButton = () => {
+        setSiteNumber(prevState => prevState - 1)
+        const offset = siteNumber * 20
+        loadData(offset);
+    }
+
+useEffect(() => {
+    loadData(0)
+}, [])
+
+return (
+    <>
+        {!dogData && <Spinner animation="border" role="status" className="position-absolute top-50 start-50">
+            <span className="visually-hidden-focusable"></span>
+        </Spinner>}
+        {dogData && dogData.map((dog, index) => (
             <Col xl={3} md={4} sm={6}>
-                {!dogData && <h1>Trwa ładowwanie</h1>}
-                {dogData && dogData.map((dog, index)=> (
                 <Card key={index} className="border-light shadow-lg mt-5 mb-5 ms-2 me-2 ">
                     <Card.Img variant="top" src={dog.image_link}/>
                     <Card.Body className="d-flex justify-content-between">
-                            <Card.Title>{dog.name}</Card.Title>
-                            <i className={`fa-regular fa-heart ${iconState ? "fa-solid" : "fa-regular"}`} onClick={onHandleIconClick}></i>
+                        <Card.Title>{dog.name}</Card.Title>
+                        <i className={`fa-regular fa-heart ${itsFavourites ? "fa-solid" : "fa-regular"}`}
+                           onClick={onHandleIconClick}></i>
                     </Card.Body>
                     <Button variant="outline-info">More</Button>
                 </Card>
-                ))}
             </Col>
+
+        ))}
+        {!dogData && <Spinner animation="border" role="status" className="position-absolute top-50 start-50">
+            <span className="visually-hidden-focusable"></span>
+        </Spinner>}
+        {dogData &&
+            <div><Button onClick={handleClickNextButton}>Next</Button>
+                <Button onClick={handleClickPrevButton}>Previous</Button> </div>}
+    </>
 );
-};
+}
+
 
 export default BreedCard;
--- src/pages/DogList/DogList.js
+++ src/pages/DogList/DogList.js
@@ -1,21 +1,22 @@
 import React, {useEffect, useState} from 'react';
 import Container from "react-bootstrap/Container";
-import {Row} from "react-bootstrap";
+import {Button, Row} from "react-bootstrap";
 import BreedCard from "../../components/BreedCard/BreedCard";
-
+const apiUrl = "https://api.api-ninjas.com/v1/dogs?min_height=1";
+const apiKey = "Puy2zDCkC5Og1tl/a9AkYQ==duVwVJvXcT0MnzNG";
 const DogList = () => {
 
 
     return (
         <>
+
             <Container fluid className="pages__container main h-100 shadow-lg bg-white " style={{overflowY: "scroll"}}>
                 <Container fluid className="h-100 bg-white">
-                    <Row className="d-flex">
+                    <Row className="d-flex flex-wrap">
                             <BreedCard/>
                     </Row>
                 </Container>
             </Container>
-
         </>
 );
 };

25fb4e615eab9b85e3909ab20ca10aa365c7022b
mirror86 <mira.wolicka@gmail.com>
2023-07-13 17:48:50 +0200

    radio button in component Preference Page

--- src/components/PreferencePage/PreferencePage.js
+++ src/components/PreferencePage/PreferencePage.js
@@ -14,29 +14,28 @@ const dogSilhouette = [
     "Doesn't matter"
 ]
 const PreferencePage = () => {
-    const [dogSize, setDogSize] = useState(dogSizes[0])
-    const [dogWeight, setDogWeight] = useState(dogSilhouette[0])
-
     return (
         <>
             <header>
                 <h1>What does your dream dog look like?</h1>
             </header>
-            <section>
-                <p>you can only choose one answer </p>
+            <section className="w-50 mx-auto">
+                <p>choose one of the four answers</p>
                 <Form>
                     {['radio'].map((type) => (
                         <div key={`default-${type}`} className="mb-3">
-                            {dogSizes.map((size, index) => (
-                            <Form.Check className="d-flex flex-row-reverse justify-content-between" // prettier-ignore
+                            {dogSizes.map((size) => (
+                            <Form.Check className="d-flex flex-row-reverse justify-content-between p-1" // prettier-ignore
                                         type={type}
                                         id={`default-`}
                                         label={`${size} `}
                                         name="group1"
                             />
+
                             ))}
+                            <p>choose one of the three answers</p>
                             {dogSilhouette.map((silhouette, index) => (
-                                <Form.Check className="d-flex flex-row-reverse justify-content-between" // prettier-ignore
+                                <Form.Check className="d-flex flex-row-reverse justify-content-between p-1" // prettier-ignore
                                 type={type}
                             id={`default-`}
                             label={`${silhouette} `}
--- src/components/QuestionnairePage/QuestionnairePage.js
+++ src/components/QuestionnairePage/QuestionnairePage.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import FormRange from "../FormRange/FormRange";
-import {Button} from "react-bootstrap";
 
 
 

70e7a6d19137d5639141e6aff5443d4a649c2f94
mirror86 <mira.wolicka@gmail.com>
2023-07-20 19:26:18 +0200

    label change

--- src/components/Anwsers/Answers.js
+++ src/components/Anwsers/Answers.js
@@ -7,7 +7,7 @@ const Answers = ({sentData}) => {
         <>
         <div className="finder__wrapper result d-flex justify-content-end flex-column">
             <div className="result"></div>
-            <Button variant="outline-info" size="lg" className="ms-2 me-2 mt-3 mb-4 " onClick={sentData}>Meet Your dream dog</Button>
+            <Button variant="outline-info" size="lg" className="ms-2 me-2 mt-3 mb-4 " onClick={sentData}> Click to meet Your dream dog</Button>
         </div>
 
         </>

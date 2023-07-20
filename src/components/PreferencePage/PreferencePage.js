7cc8dc374d14019547ef269bf4c96e86c618ff0b
mirror86 <mira.wolicka@gmail.com>
2023-07-20 14:35:03 +0200

    style changes

--- src/components/PreferencePage/PreferencePage.js
+++ src/components/PreferencePage/PreferencePage.js
@@ -3,7 +3,6 @@ import {Button, Form} from "react-bootstrap";
 import {dogHeightPreferences, dogWeightPreferences} from "../../data";
 
 
-
 const PreferencePage = ({onOtherQuestions, weightPreferencesMustHave, heightPreferencesMustHave}) => {
 
     return (
@@ -17,32 +16,34 @@ const PreferencePage = ({onOtherQuestions, weightPreferencesMustHave, heightPref
                     {Object.entries(dogHeightPreferences).map(([key, preference]) => (
                         <div key={`height-${key}`} className="mb-3">
                             <Form.Check style={{border: "1px solid lightgrey", borderRadius: "5px"}}
-                                className="d-flex flex-row-reverse justify-content-between p-3 ms-3 me-5"
-                                type="radio"
-                                id={`height-${key}`}
-                                label={preference.label}
-                                name="group1"
-                                onChange={() =>heightPreferencesMustHave({key})}
+                                        className="d-flex flex-row-reverse justify-content-between p-3 ms-3 me-5"
+                                        type="radio"
+                                        id={`height-${key}`}
+                                        label={preference.label}
+                                        name="group1"
+                                        onChange={() => heightPreferencesMustHave({key})}
                             />
-                        </div>  ))}
+                        </div>))}
                     <p>CHOOSE ONE OF THE THREE ANSWERS</p>
-                            {Object.entries(dogWeightPreferences).map(([key, preference]) => (
-                                <div key={`weight-${key}`} className="mb-3">
-                                    <Form.Check style={{border: "1px solid lightgrey", borderRadius: "5px"}}
+                    {Object.entries(dogWeightPreferences).map(([key, preference]) => (
+                        <div key={`weight-${key}`} className="mb-3">
+                            <Form.Check style={{border: "1px solid lightgrey", borderRadius: "5px"}}
                                         className="d-flex flex-row-reverse justify-content-between p-3 ms-3 me-5"
                                         type="radio"
                                         id={`weight-${key}`}
                                         label={preference.label}
                                         name="group2"
-                                        onChange={()=> weightPreferencesMustHave({key})}
-                                    />
+                                        onChange={() => weightPreferencesMustHave({key})}
+                            />
                         </div>
                     ))}
                 </Form>
-                        <div className="align-self-center">
-                        <Button href="/" variant="outline-info" size="lg" className="ms-2 me-2 mt-3">Back to home page</Button>
-                        <Button variant="outline-info" size="lg" className="ms-2 me-2 mt-3" onClick={onOtherQuestions}>Next</Button>
-            </div>
+                <div className="align-self-center">
+                    <Button href="/" variant="outline-info" size="lg" className="ms-2 me-2 mt-3">Back to home
+                        page</Button>
+                    <Button variant="outline-info" size="lg" className="ms-2 me-2 mt-3"
+                            onClick={onOtherQuestions}>Next</Button>
+                </div>
             </section>
 
         </>

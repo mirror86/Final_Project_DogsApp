7553c9c84b52a5113f33378085fb3fd0d0fba5ad
mirror86 <mira.wolicka@gmail.com>
2023-07-21 16:22:16 +0200

    added information when FavDogs is Empty

--- src/components/EmptyFavElement/EmptyFavElement.js
+++ src/components/EmptyFavElement/EmptyFavElement.js
@@ -3,9 +3,9 @@ import Container from "react-bootstrap/Container";
 
 const EmptyFavElement = () => {
     return (
-        <Container className="text-center">
+        <Container className="text-center ">
             <div className="fav-empty"></div>
-            <h1 className="p-3">There are no dogs here</h1>
+            <h1>There are no dogs here</h1>
         </Container>
     );
 };
